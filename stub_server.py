ac1a9b0a7de8b35ddcc5d42315bf72c4c7980e7c
Eduardo Arias <eduarias@users.noreply.github.com>
2018-09-24 15:21:42 +0200

    endpoint to log calls

--- stub_server.py
+++ stub_server.py
@@ -29,6 +29,12 @@ def choose_response(response_code):
     return str(request), response_code
 
 
+@app.route('/log')
+def log():
+    app.logger.info(f'Headers:\n{request.headers}\n--------\n{request.get_json}')
+    return 'Ok'
+
+
 def get_lognormal_response_time(mu=config.lognormal_mu, sigma=config.lognormal_sigma):
     return int(np.random.lognormal(mu, sigma, 1)[0])/1000.0
 
